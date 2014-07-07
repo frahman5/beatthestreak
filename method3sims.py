41c7f875f2ea54cbb38b6a5ed45bfa4c7dd8415b
Faiyam Rahman <frahman305@gmail.com>
2014-07-07 15:23:15 +0000

    method 3 round 1 last 3rd sims

--- method3sims.py
+++ method3sims.py
@@ -5,11 +5,9 @@ from beatthestreak.npsimulation import NPSimulation as NPS
 ## Bogus numbers used in initialization.
 sim = NPS(2010, 2009, 20, 20) # bogus numbers, just to initalize
 
+#sim.mass_simulate(
+ #   (2010, 2010), (1, 1), (1, 300), (201, 250), (100, 600), 
+  #  minERARange=(2.0, 4.0), method=3)
 sim.mass_simulate(
-    (2010, 2010), (1, 1), (1, 300), (101, 150), (100, 600), 
+    (2010, 2010), (1, 1), (1, 300), (251, 300), (100, 600), 
     minERARange=(2.0, 4.0), method=3)
-sim.mass_simulate(
-    (2010, 2010), (1, 1), (1, 300), (151, 200), (100, 600), 
-    minERARange=(2.0, 4.0), method=3)
-
-## Left to do: P(201-300)
