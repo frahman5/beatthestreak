4668ddd4e84184045c8593555d978109e47cc61c
Faiyam Rahman <frahman305@gmail.com>
2014-07-07 03:32:14 +0000

    method 3 first 3rd results

--- method4sims.py
+++ method4sims.py
@@ -7,9 +7,9 @@ sim = NPS(2010, 2009, 20, 20) # bogus numbers, just to initalize
 
 sim.mass_simulate(
     (2010, 2010), (1, 1), (1, 300), (1, 50), (100, 600), 
-    minERARange=(2.0, 4.0), method=3)
+    minERARange=(2.0, 4.0), method=4)
 sim.mass_simulate(
     (2010, 2010), (1, 1), (1, 300), (51, 100), (100, 600), 
-    minERARange=(2.0, 4.0), method=3)
+    minERARange=(2.0, 4.0), method=4)
 
 ## Left to do: P(201-300)
